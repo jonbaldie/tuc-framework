8ca59a30ef9484b6f9c9ed2bb8476568165e4e32
Jonathan Baldie <jon@jonbaldie.com>
2020-12-03 17:02:56 +0000

    http

---
+++ src/Http/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tuc\Http;
+
+use Psr\Http\Message\ServerRequestInterface as Psr7Request;
+
+interface Request extends Psr7Request
+{
+    //
+}
---
+++ src/Http/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tuc\Http;
+
+use Psr\Http\Message\ResponseInterface as Psr7Response;
+
+interface Response extends Psr7Response
+{
+    //
+}
